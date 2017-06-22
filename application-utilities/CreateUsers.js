ae9579633a9d142bc9e501ed9a8c49dd03c17a62
praddumnyadav <praddumn.kumar@oodlestechnologies.com>
2017-06-22 16:33:42 +0530

    change for server node

--- application-utilities/CreateUsers.js
+++ application-utilities/CreateUsers.js
@@ -1,5 +1,7 @@
 'use strict';
 var fs = require('fs');
+var os = require('os');
+var ifaces = os.networkInterfaces();
 
 //azureQueue.deleteTopic('account-create');
 //azureQueue.deleteTopic('account-result');
@@ -20,33 +22,58 @@ setInterval(function() {
 //     getUsersResultFromqueue();
 // }, 10)
 
+var serverip = ifaces.eth0[0].address;
+
+var serverNode = getserverNode();
+
+
+function getserverNode() {
+    switch (serverip) {
+        case '10.0.0.4':
+            return '1';
+            break;
+        case '22':
+            return '2';
+            break;
+        case '33':
+            return '3';
+            break;
+        case '44':
+            return '4';
+            break;
+        default:
+            return 'None';
+    }
+}
+
+
+
 var userCount = 0;
+
 function getUsersResultFromqueue() {
 
     azureQueue.receiveSubscriptionMessage('account-result', 'result', (error, receivedMessage) => {
         if (error) {
             //    Logger.info('Error in receiving message from result', error);
         } else {
-          //  Logger.info('Message received from users result queue ', receivedMessage);
+            //  Logger.info('Message received from users result queue ', receivedMessage);
             var resultObj = JSON.parse(receivedMessage.body);
             resultObj = JSON.stringify(resultObj);
 
-          //  console.log(' result obj '+JSON.stringify(resultObj,null,2));
+            //  console.log(' result obj '+JSON.stringify(resultObj,null,2));
             userCount++;
-            fs.appendFile("CreateAccountResult", userCount+'. '+resultObj+'\n', function(err) {
+            fs.appendFile("CreateAccountResult", userCount + '. ' + resultObj + '\n', function(err) {
                 if (err) {
                     Logger.info(' error in writing to file ');
                     return console.log(err);
-                }
-                else
-                {
-                  console.log("CreateAccount result written to file");
-                  azureQueue.deleteMessage(receivedMessage, function(deleteError) {
-                      if (!deleteError) {
-                          // Message deleted
-                        //  console.log('message has been deleted from user result subscription');
-                      }
-                  })
+                } else {
+                    console.log("CreateAccount result written to file");
+                    azureQueue.deleteMessage(receivedMessage, function(deleteError) {
+                        if (!deleteError) {
+                            // Message deleted
+                            //  console.log('message has been deleted from user result subscription');
+                        }
+                    })
                 }
             });
         }
@@ -62,29 +89,30 @@ function createUsersFromqueue() {
             //    Logger.info(' Message received from queue to create user account ', receivedMessage);
             var recordObj1 = JSON.parse(receivedMessage.body);
 
-        //    Logger.info(' recordObj1 -- ',recordObj1);
+            //    Logger.info(' recordObj1 -- ',recordObj1);
 
             privateWeb3.personal.newAccount(recordObj1.ethPassword, function(error, result) {
                 if (!error) {
                     domain.User.query().insert({
                         my4chainId: recordObj1.my4chainId,
                         ethPassword: recordObj1.ethPassword,
+                        serverNode: serverNode,
                         accountAddress: result
                     }).then(function(data) {
                         Logger.info('Account Successfully created and saved in database ');
                         var message = {
-                                my4chainId: recordObj1.my4chainId,
-                                accountAddress: result
+                            my4chainId: recordObj1.my4chainId,
+                            accountAddress: result
                         }
 
-                        console.log(' stringified - '+JSON.stringify(message));
+                        console.log(' stringified - ' + JSON.stringify(message));
                         azureQueue.sendTopicMessage('account-result', JSON.stringify(message), (error) => {
                             if (!error) {
-                               Logger.info(' message sent to result queue');
+                                Logger.info(' message sent to result queue');
                                 azureQueue.deleteMessage(receivedMessage, function(deleteError) {
                                     if (!deleteError) {
                                         // Message deleted
-                                      console.log('message has been deleted from account-create queue ');
+                                        console.log('message has been deleted from account-create queue ');
                                     }
                                 })
                             } else {
@@ -95,7 +123,7 @@ function createUsersFromqueue() {
 
                     privateWeb3.personal.unlockAccount(result, recordObj1.ethPassword, 0, function(error, result1) {
                         if (!error) {
-                           Logger.info('New account ', result, ' unlocking success, will reamin unlocked till geth running ', result1);
+                            Logger.info('New account ', result, ' unlocking success, will reamin unlocked till geth running ', result1);
                         } else {
                             Logger.info(' New account unlocking failed', error);
                         }
