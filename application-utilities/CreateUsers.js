9e43b6c3db562f96f24fae1780dd7e8ef65e15d7
praddumnyadav <praddumn.kumar@oodlestechnologies.com>
2017-07-18 17:05:58 +0530

    changes

--- application-utilities/CreateUsers.js
+++ application-utilities/CreateUsers.js
@@ -90,46 +90,76 @@ function createUsersFromqueue() {
 
             privateWeb3.personal.newAccount(recordObj1.ethPassword, function(error, result) {
                 if (!error) {
-                    domain.User.query().insert({
-                        my4chainId: recordObj1.my4chainId,
-                        ethPassword: recordObj1.ethPassword,
-                        serverNode: serverNode,
-                        accountAddress: result
-                    }).then(function(data) {
-                        Logger.info('Account Successfully created and saved in database ');
-                        var message = {
-                            my4chainId: recordObj1.my4chainId,
-                            accountAddress: result
-                        }
 
-                        console.log(' stringified - ' + JSON.stringify(message));
-                        azureQueue.sendTopicMessage('account-result', JSON.stringify(message), (error) => {
-                            if (!error) {
-                                Logger.info('Message sent to result queue');
 
-                                privateWeb3.eth.sendTransaction({
-                                    from: privateWeb3.eth.coinbase,
-                                    to: result,
-                                    value: privateWeb3.toWei(2, 'ether')
-                                }, (tx_error, tx_result) => {
-                                    if (!tx_error) {
-                                        Logger.info("Payment of 2 ether to account success ", tx_result);
-                                    } else {
-                                        Logger.info("Payment of 2 ether to account failed ", tx_error);
-                                    }
-                                });
+                  privateWeb3.eth.sendTransaction({
+                      from: privateWeb3.eth.coinbase,
+                      to: result,
+                      value: privateWeb3.toWei(2, 'ether')
+                  }, (tx_error, tx_result) => {
+                      if (!tx_error) {
+                          Logger.info("Payment of 2 ether to account success ", tx_result);
 
-                                azureQueue.deleteMessage(receivedMessage, function(deleteError) {
-                                    if (!deleteError) {
-                                        // Message deleted
-                                        console.log('Message has been deleted from account-create queue ');
-                                    }
-                                })
-                            } else {
-                                Logger.info('Error in sending to result queue ', error);
-                            }
-                        })
-                    });
+                          domain.User.query().insert({
+                              my4chainId: recordObj1.my4chainId,
+                              ethPassword: recordObj1.ethPassword,
+                              serverNode: serverNode,
+                              accountAddress: result
+                          }).then(function(data) {
+                              Logger.info('Account Successfully created and saved in database ');
+                              var message = {
+                                  my4chainId: recordObj1.my4chainId,
+                                  accountAddress: result
+                              }
+
+                              console.log(' stringified - ' + JSON.stringify(message));
+                              azureQueue.sendTopicMessage('account-result', JSON.stringify(message), (error) => {
+                                  if (!error) {
+                                      Logger.info('Message sent to result queue');
+                                      azureQueue.deleteMessage(receivedMessage, function(deleteError) {
+                                          if (!deleteError) {
+                                              // Message deleted
+                                              console.log('Message has been deleted from account-create queue ');
+                                          }
+                                      })
+                                  } else {
+                                      Logger.info('Error in sending to result queue ', error);
+                                  }
+                              })
+                          });
+                      } else {
+                          Logger.info("Payment of 2 ether to account failed ", tx_error);
+                          domain.User.query().insert({
+                              my4chainId: recordObj1.my4chainId,
+                              ethPassword: recordObj1.ethPassword,
+                              serverNode: serverNode,
+                              accountAddress: result
+                          }).then(function(data) {
+                              Logger.info('Account Successfully created and saved in database ');
+                              var message = {
+                                  my4chainId: recordObj1.my4chainId,
+                                  accountAddress: result
+                              }
+
+                              console.log(' stringified - ' + JSON.stringify(message));
+                              azureQueue.sendTopicMessage('account-result', JSON.stringify(message), (error) => {
+                                  if (!error) {
+                                      Logger.info('Message sent to result queue');
+                                      azureQueue.deleteMessage(receivedMessage, function(deleteError) {
+                                          if (!deleteError) {
+                                              // Message deleted
+                                              console.log('Message has been deleted from account-create queue ');
+                                          }
+                                      })
+                                  } else {
+                                      Logger.info('Error in sending to result queue ', error);
+                                  }
+                              })
+                          });
+                      }
+                  });
+
+
                     // privateWeb3.personal.unlockAccount(result, recordObj1.ethPassword, 0, function(error, result1) {
                     //     if (!error) {
                     //         Logger.info('New account ', result, ' unlocking success, will reamin unlocked till geth running ', result1);
