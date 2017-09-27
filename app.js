8afd0f33fc5e766a6767923b0091592b5e7f6a1f
deepchand789 <deepchand.prajapati@oodlestechnologies.com>
2017-09-27 18:05:50 +0530

    formated and cleaned code

--- app.js
+++ app.js
@@ -4,87 +4,55 @@
 
 var mongoosemask = require('mongoosemask');
 global.configurationHolder = require('./configurations/DependencyInclude.js');
-
 global.app = module.exports = express();
-
-///
 var bodyParser = require('body-parser');
-//app.use(express.json());
-
-app.use(bodyParser.json({limit: '52428800',type: 'application/json'}));
-app.use(bodyParser.urlencoded({limit: '52428800',extended: true,parameterLimit:52428800}));
+app.use(bodyParser.json({
+    limit: '52428800',
+    type: 'application/json'
+}));
+app.use(bodyParser.urlencoded({
+    limit: '52428800',
+    extended: true,
+    parameterLimit: 52428800
+}));
 app.use(bodyParser());
-
-
-
-
-
 app.use(errorHandler());
-
-///
-
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.urlencoded({ extended: false }));
-//
-// app.use(bodyParser.json());
-//
-// app.use(errorHandler());
-
-
 global.router = express.Router();
 global.publicdir = __dirname;
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/upload'));
-//global.route = require('./configurations/routes');
-
-// require('./configurations/EthereumContract.js');
-//removing fields from the response
-app.use(mongoosemask(function (result, mask, done) {
-	var masked = mask(result, []);
-	if (masked.object) {
-//		masked.object = mask(result.object, ['__v', 'salt', 'password']);
-	}
-	done(null, masked);
+app.use(mongoosemask(function(result, mask, done) {
+    var masked = mask(result, []);
+    if (masked.object) {}
+    done(null, masked);
 }));
-
-process.on('uncaughtException', function (err) {
-  console.error(err);
-	Logger.info("error uncaughtException",err);
-
+process.on('uncaughtException', function(err) {
+    console.error(err);
+    Logger.info("error uncaughtException", err);
 });
-
-app.get("/",function(req,res){
-res.send("server working.");
+app.get("/", function(req, res) {
+    res.send("server working.");
 });
-
 Layers = require('./application-utilities/layers').Express;
 var wiring = require('./configurations/UrlMapping');
 new Layers(app, router, __dirname + '/application/controller-service-layer', wiring);
-
-// error handler, required as of 0.3.0
-app.use(function(err, req, res, next){
-	var errorObject = {};
-	Logger.error("Error in express ",err.stack)
-	var errorString= JSON.stringify(err);
-	console.log(errorString.length,errorString)
-	if(errorString.length < 5){
-		errorObject.message = "Error in request api";
-		errorObject.error = true;
-		errorObject.object = null;
-		errorObject.timeStamp = new Date().getTime();
-		  res.status(500).json(errorObject);
-	}else{
-       err.object =err.errors;
-			 err.error=true;
-			 err.timeStamp = new Date().getTime();
-			 delete err.errors;
-		  res.status(400).json(err);
-	}
-//	console.log("Error in express after stringfy ",errorJson)
-	//res.send(err)
-	//if()
-
+app.use(function(err, req, res, next) {
+    var errorObject = {};
+    Logger.error("Error in express ", err.stack)
+    var errorString = JSON.stringify(err);
+    console.log(errorString.length, errorString)
+    if (errorString.length < 5) {
+        errorObject.message = "Error in request api";
+        errorObject.error = true;
+        errorObject.object = null;
+        errorObject.timeStamp = new Date().getTime();
+        res.status(500).json(errorObject);
+    } else {
+        err.object = err.errors;
+        err.error = true;
+        err.timeStamp = new Date().getTime();
+        delete err.errors;
+        res.status(400).json(err);
+    }
 });
-//app.use('/',router)
-
 configurationHolder.Bootstrap.initApp();
--- configurations/UrlMapping.js
+++ configurations/UrlMapping.js
@@ -1,11 +1,8 @@
 	module.exports = function(app) {
 	    var controllers = app.controllers,
 	        views = app.views;
-
 	    return {
-
-	    // ------------ Create Account
-	        // create account of the system
+	        // 1 Create Account
 	        "/api/v1/contract/createaccount": [{
 	            method: "POST",
 	            action: controllers.privateEthereumController.createAccount,
@@ -14,27 +11,24 @@
 	                json: views.jsonView
 	            }
 	        }],
-					// create account of the system
-	        "/api/v1/contract/createimportAccount": [{
-	            method: "POST",
-	            action: controllers.privateEthereumController.createimportAccount,
-	            middleware: [validater(validationFile.createAccount)],
-	            views: {
-	                json: views.jsonView
-	            }
-	        }],
-
- /* ---------------------------------------------------------------------------- */
-		// transaction confirmation and detail
-	        // It tell about confirmation of the contract
-	        "/api/v1/contract/txconfirmations": [{
+					// 2 find the balance of account
+	        "/api/v1/contract/Balance": [{
 	            method: "GET",
-	            action: controllers.privateEthereumController.transactionConfirmations,
+	            action: controllers.privateEthereumController.accountBalance,
 	            views: {
 	                json: views.jsonView
 	            }
 	        }],
-	        // get the transaction detail
+					// 3	Send hash in transaction
+					"/api/v1/contract/sendHashIntransaction": [{
+							method: "POST",
+							action: controllers.privateEthereumController.sendHashIntransaction,
+							middleware: [validater(validationFile.sendHashIntransaction)],
+							views: {
+									json: views.jsonView
+							}
+					}],
+					// 4	get the transaction detail
 	        "/api/v1/contract/transactiondetail": [{
 	            method: "GET",
 	            action: controllers.privateEthereumController.transactionDetail,
@@ -42,28 +36,24 @@
 	                json: views.jsonView
 	            }
 	        }],
- /* ----------------------------------------------------------------------------------  */
-	    // ----------- contract api
-
-				  // create contract
-				  "/api/v1/contract/createcontract": [{
-				      method: "POST",
-				      action: controllers.privateEthereumController.smartContract,
-				      middleware: [validater(validationFile.privateCreateContract)],
-				      views: {
-				          json: views.jsonView
-				      }
-				    }],
-						// create RAW contract
-					  "/api/v1/contract/createrawcontract": [{
-					      method: "POST",
-					      action: controllers.privateEthereumController.smartRawContract,
-					      middleware: [validater(validationFile.privateCreateContract)],
-					      views: {
-					          json: views.jsonView
-					      }
-					    }],
-	        // excute the function of contract
+	        // 5	transaction confirmation and detail
+	        "/api/v1/contract/txconfirmations": [{
+	            method: "GET",
+	            action: controllers.privateEthereumController.transactionConfirmations,
+	            views: {
+	                json: views.jsonView
+	            }
+	        }],
+	        // 6	create contract
+	        "/api/v1/contract/createcontract": [{
+	            method: "POST",
+	            action: controllers.privateEthereumController.smartContract,
+	            middleware: [validater(validationFile.privateCreateContract)],
+	            views: {
+	                json: views.jsonView
+	            }
+	        }],
+	        // 7	excute the function of contract
 	        "/api/v1/contract/assignandremove": [{
 	            method: "POST",
 	            action: controllers.privateEthereumController.assignandremove,
@@ -72,8 +62,7 @@
 	                json: views.jsonView
 	            }
 	        }],
-
-	        // review the contract
+	        // 8	review the contract
 	        "/api/v1/contract/review": [{
 	            method: "POST",
 	            action: controllers.privateEthereumController.review,
@@ -82,8 +71,7 @@
 	                json: views.jsonView
 	            }
 	        }],
-
-	        // revoke the contract
+	        // 9	revoke the contract
 	        "/api/v1/contract/revoke": [{
 	            method: "POST",
 	            action: controllers.privateEthereumController.revoke,
@@ -92,8 +80,7 @@
 	                json: views.jsonView
 	            }
 	        }],
-
-	        // change state of contract ACK , DECLINE ,
+	        // 10	change state of contract ACK , DECLINE ,
 	        "/api/v1/contract/changestate": [{
 	            method: "POST",
 	            action: controllers.privateEthereumController.changestate,
@@ -102,43 +89,20 @@
 	                json: views.jsonView
 	            }
 	        }],
-					// ,
-	        "/api/v1/contract/sendHashIntransaction": [{
-	            method: "POST",
-	            action: controllers.privateEthereumController.sendHashIntransaction,
-	            middleware: [validater(validationFile.sendHashIntransaction)],
-	            views: {
-	                json: views.jsonView
-	            }
-	        }],
-					"/api/v1/contract/broadcastTransactions": [{
-	            method: "POST",
-	            action: controllers.privateEthereumController.broadcastTransactions,
+	        // 11	change state of contract ACK , DECLINE ,
+	        "/api/v1/contract/getHashIntransaction": [{
+	            method: "GET",
+	            action: controllers.privateEthereumController.getHashIntransaction,
 	            middleware: [],
 	            views: {
 	                json: views.jsonView
 	            }
 	        }],
-					// change state of contract ACK , DECLINE ,
-					"/api/v1/contract/getHashIntransaction": [{
-							method: "GET",
-							action: controllers.privateEthereumController.getHashIntransaction,
-							middleware: [],
-							views: {
-									json: views.jsonView
-							}
-					}],
-	        // get the detail of the contract
-	        "/api/v1/contract/userdetail": [{
-	            method: "POST",
-	            action: controllers.privateEthereumController.userdetail,
-	            middleware: [validater(validationFile.userdetail)],
-	            views: {
-	                json: views.jsonView
-	            }
-	        }],
 
-	        // log for the contract
+// -------------------------------------------------------------------------------------------------------------//
+
+//   Below APIs currently not being used
+	        // 1 	log for the contract
 	        "/api/v1/contract/log": [{
 	            method: "POST",
 	            action: controllers.privateEthereumController.log,
@@ -147,7 +111,7 @@
 	                json: views.jsonView
 	            }
 	        }],
-	        // log for file modify in the contract
+	        // 2	log for file modify in the contract
 	        "/api/v1/contract/fileModifylog": [{
 	            method: "POST",
 	            action: controllers.privateEthereumController.fileModifylog,
@@ -156,11 +120,9 @@
 	                json: views.jsonView
 	            }
 	        }],
-
-	  /* --------------------------------------------------------------------------------------- */
-	    // explorer api
-
-	        // use for fetch data for the user
+	        /* --------------------------------------------------------------------------------------- */
+	        // explorer api
+	        // 3	se for fetch data for the user
 	        "/api/v1/contractPublish/fetchData": [{
 	            method: "GET",
 	            action: controllers.publishDataController.fetchData,
@@ -168,7 +130,7 @@
 	                json: views.jsonView
 	            }
 	        }],
-	        // get detail of batch id  (batchid is a  id given when transaction is publish)
+	        // 4	get detail of batch id  (batchid is a  id given when transaction is publish)
 	        "/api/v1/contractPublish/batchData/:id": [{
 	            method: "GET",
 	            action: controllers.publishDataController.batchData,
@@ -176,11 +138,9 @@
 	                json: views.jsonView
 	            }
 	        }],
-
-	  /* --------------------------------------------------------------------------------------- */
-	      // get data from the message queue
-
-	        // used when we have azure message queue to receive the message
+	        /* --------------------------------------------------------------------------------------- */
+	        // get data from the message queue
+	        // 5	used when we have azure message queue to receive the message
 	        "/api/v1/queue/reciveMessageQueue": [{
 	            method: "GET",
 	            action: controllers.messageQueueController.reciveMessageQueue,
@@ -188,8 +148,7 @@
 	                json: views.jsonView
 	            }
 	        }],
-
-	        // used to  send the data to the azure message queue
+	        // 6	used to  send the data to the azure message queue
 	        "/api/v1/queue/sendToQueue": [{
 	            method: "GET",
 	            action: controllers.messageQueueController.sendMessageQueue,
@@ -197,31 +156,19 @@
 	                json: views.jsonView
 	            }
 	        }],
-
-	    /* ------------------------------------------------------------------------------------ */
-
-	        // find the balance of account
-	        "/api/v1/contract/Balance": [{
-	            method: "GET",
-	            action: controllers.privateEthereumController.accountBalance,
+	        /* ------------------------------------------------------------------------------------ */
+	        // 7	send ether to other account
+	        "/api/v1/contract/sendether/:requestid": [{
+	            method: "POST",
+	            action: controllers.privateEthereumController.privateSendether,
+	            middleware: [validater(validationFile.privateSendether)],
 	            views: {
 	                json: views.jsonView
 	            }
 	        }],
-
-
-	        // send ether to other account
-	        "/api/v1/contract/sendether/:requestid": [{
-	                method: "POST",
-	                action: controllers.privateEthereumController.privateSendether,
-	                middleware: [validater(validationFile.privateSendether)],
-	                views: {
-	                    json: views.jsonView
-	                }
-	            }],
-	/* -------------------------------------------------------------------------------------- */
-        // developer use
-	            // dummy api just for developer to assign balance in multiple account . not for production
+	        /* -------------------------------------------------------------------------------------- */
+	        // developer use
+	        // 8	dummy api just for developer to assign balance in multiple account . not for production
 	        "/api/v1/insertBalance": [{
 	            method: "GET",
 	            action: controllers.privateEthereumController.insertBalance,
@@ -229,7 +176,7 @@
 	                json: views.jsonView
 	            }
 	        }],
-	        // don't need it in production
+	        // 9	don't need it in production
 	        "/api/v1/publishdata": [{
 	            method: "POST",
 	            action: controllers.publishDataController.sendData,
@@ -237,6 +184,7 @@
 	                json: views.jsonView
 	            }
 	        }],
+					// 10
 	        "/api/v1/contract/testContract": [{
 	            method: "GET",
 	            action: controllers.privateEthereumController.testContract,
@@ -244,6 +192,16 @@
 	                json: views.jsonView
 	            }
 	        }],
+					// 11	get the detail of the contract
+	        "/api/v1/contract/userdetail": [{
+	            method: "POST",
+	            action: controllers.privateEthereumController.userdetail,
+	            middleware: [validater(validationFile.userdetail)],
+	            views: {
+	                json: views.jsonView
+	            }
+	        }],
+					// 12
 	        "/api/v1/requestconfirmation/:requestid": [{
 	            method: "GET",
 	            action: controllers.privateEthereumController.requestConfirmation,
@@ -251,133 +209,40 @@
 	                json: views.jsonView
 	            }
 	        }],
+					// 13
 	        "/api/v1/chain/latestBlockdata": [{
-	                method: "GET",
-	                action: controllers.privateEthereumController.latestBlock,
-	                views: {
-	                    json: views.jsonView
-	                }
-
-	            }]
-	            // "/api/v1/getpublishdata": [{
-	            //     method: "GET",
-	            //     action: controllers.publishDataController.getData,
-	            //     views: {
-	            //         json: views.jsonView
-	            //     }
-	            // }],
-	            // "/api/v1/checkForRequest": [{
-	            // 		method: "GET",
-	            // 		action: controllers.privateEthereumController.checkForRequest,
-	            // 		views: {
-	            // 				json: views.jsonView
-	            // 		}
-	            // }],
-	            // ,"/api/coinbase/balance": [{
-	            //     method: "GET",
-	            //     action: controllers.coinbaseController.notifications,
-	            //     views: {
-	            //         json: views.jsonView
-	            // }]
-	            //     }
-	            // ,	"/api/v1/sendether": [{
-	            // 		method: "POST",
-	            // 		action: controllers.ethereumController.createTransaction,
-	            // 		views: {
-	            // 			json: views.jsonView
-	            // 		}
-	            // 	}
-	            // ]
-	            // 		,
-	            // "/api/coinbase/accounts": [{
-	            // 				method: "GET",
-	            // 				action: controllers.coinbaseController.notifications,
-	            // 				views: {
-	            // 										 json: views.jsonView
-	            // 								}
-	            // 			 }],
-	            //
-	            //
-	            // "/api/coinbase/createAccount": [{
-	            // 			 method: "GET",
-	            // 			 action: controllers.coinbaseController.createAccount,
-	            // 			 views: {
-	            // 								json: views.jsonView
-	            // 							}
-	            // 				}]
-	            // 			,"/api/v1/balance": [{
-	            // 	method: "POST",
-	            // 	action: controllers.ethereumController.accountBalance,
-	            // 	views: {
-	            // 		json: views.jsonView
-	            // 	}
-	            // }
-	            // ],
-	            // "/api/v1/coinbaseBalance": [{
-	            // 	method: "GET",
-	            // 	action: controllers.privateEthereumController.coinbaseBalance,
-	            // 	views: {
-	            // 		json: views.jsonView
-	            // 	}
-	            // }
-	            // ]
-	            // ,"/api/v1/createaccount": [{
-	            // 		method: "POST",
-	            // 		action: controllers.ethereumController.createAccount,
-	            // 		views: {
-	            // 			json: views.jsonView
-	            // 		}
-	            // 	}
-	            // ]
-	            // ,
-	            // "/api/v1/saveFileAndGenerateHash": [{
-	            // 		method: "POST",
-	            // 		action: controllers.privateEthereumController.saveFileAndGenerateHash,
-	            // 		views: {
-	            // 			json: views.jsonView
-	            // 		}
-	            // 	}
-	            // ]
-	            // ,"/api/v1/txConfirmations": [{
-	            // 		method: "POST",
-	            // 		action: controllers.ethereumController.transactionConfirmations,
-	            // 		views: {
-	            // 			json: views.jsonView
-	            // 		}
-	            // 	}
-	            // ]
-	            // ,	"/api/v1/sendether": [{
-	            // 		method: "POST",
-	            // 		action: controllers.ethereumController.createTransaction,
-	            // 		views: {
-	            // 			json: views.jsonView
-	            // 		}
-	            // 	}
-	            // ]
-	            // ,	"/api/v1/transactionDetail": [{
-	            // 			method: "POST",
-	            // 			action: controllers.ethereumController.transactionDetail,
-	            // 			views: {
-	            // 				json: views.jsonView
-	            // 			}
-	            // 		}
-	            // 	]
-	            //,
-	            // "/api/v1/privateImageHashGenerate": [{
-	            // 		method: "POST",
-	            // 		action: controllers.privateEthereumController.privateImageHashGenerate,
-	            // 		middleware: [multipartMiddleware],
-	            // 		views: {
-	            // 				json: views.jsonView
-	            // 		}
-	            // }],
-	            // "/api/v1/privateImageHashtoContract": [{
-	            // 		method: "POST",
-	            // 		action: controllers.privateEthereumController.privateImageHashtoContract,
-	            // 		middleware: [multipartMiddleware],
-	            // 		views: {
-	            // 				json: views.jsonView
-	            // 		}
-	            // }]
+	            method: "GET",
+	            action: controllers.privateEthereumController.latestBlock,
+	            views: {
+	                json: views.jsonView
+	            }
+	        }],
+					// 14	create account of the system
+					"/api/v1/contract/createimportAccount": [{
+							method: "POST",
+							action: controllers.privateEthereumController.createimportAccount,
+							middleware: [validater(validationFile.createAccount)],
+							views: {
+									json: views.jsonView
+							}
+					}],
+	        // 15	create RAW contract
+	        "/api/v1/contract/createrawcontract": [{
+	            method: "POST",
+	            action: controllers.privateEthereumController.smartRawContract,
+	            middleware: [validater(validationFile.privateCreateContract)],
+	            views: {
+	                json: views.jsonView
+	            }
+	        }],
+					// 16
+	        "/api/v1/contract/broadcastTransactions": [{
+	            method: "POST",
+	            action: controllers.privateEthereumController.broadcastTransactions,
+	            middleware: [],
+	            views: {
+	                json: views.jsonView
+	            }
+	        }]
 	    };
 	};
