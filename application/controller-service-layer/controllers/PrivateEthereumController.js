7b55965f4951a6151c6b7ae470530941adefd115
deepchand789 <deepchand.prajapati@oodlestechnologies.com>
2017-09-27 18:11:00 +0530

    code format in controller

--- application/controller-service-layer/controllers/PrivateEthereumController.js
+++ application/controller-service-layer/controllers/PrivateEthereumController.js
@@ -1,65 +1,50 @@
 var encrypt = require('../../../application-utilities/EncryptionUtility');
 var publishData = require('../../../application-utilities/cron/PromisePublishContractData');
 module.exports = function() {
-
     var accountBalance = function(req, res, callback) {
         var address = req.query.address;
         Logger.info("address", address);
         this.services.privateEthereumDetail.accountBalance(address, res, callback);
-
-        //ethereumService
     }
-
     var createAccount = function(req, res, callback) {
-        //	var password=req.body.password;
-        // var one=this.services.createAccount.createUserIndatabase(req.body);
-        // console.log(one);
         this.services.accounts.createAccount(req.body, res, callback);
     }
     var createimportAccount = function(req, res, callback) {
-        //	var password=req.body.password;
-        // var one=this.services.createAccount.createUserIndatabase(req.body);
-        // console.log(one);
         this.services.accounts.createimportAccount(req.body, res, callback);
     }
-   // function to test contract for developer
-    var testContract = function(req,res,callback){
+    // function to test contract for developer
+    var testContract = function(req, res, callback) {
         publishData.callFunction(callback);
-
     }
     // function to get block detail
-    var latestBlock = function(req, res, callback){
-      return callback(null,privateWeb3.eth.getBlock(req.query.block));
+    var latestBlock = function(req, res, callback) {
+        return callback(null, privateWeb3.eth.getBlock(req.query.block));
     }
-
-   // function to create smart contract
+    // function to create smart contract
     var smartContract = function(req, res, callback) {
         Logger.info("inside controller contract");
         Logger.info(req.body.startfrom);
-         req.body.Raw = false;
+        req.body.Raw = false;
         this.services.privateEthereumService.smartContract(req, res, callback);
     }
     // function to create smart contract
-     var smartRawContract = function(req, res, callback) {
-         Logger.info("inside controller contract");
-         req.body.Raw = true;
-         Logger.info(req.body.startfrom);
-         this.services.privateEthereumService.smartContract(req, res, callback);
-     }
-
-  // assign and remove action from user
+    var smartRawContract = function(req, res, callback) {
+        Logger.info("inside controller contract");
+        req.body.Raw = true;
+        Logger.info(req.body.startfrom);
+        this.services.privateEthereumService.smartContract(req, res, callback);
+    }
+    // assign and remove action from user
     var assignandremove = function(req, res, callback) {
-      Logger.info("inside assignandremove contract role");
-      Logger.info(new Date());
+        Logger.info("inside assignandremove contract role");
+        Logger.info(new Date());
         if (req.params) {
             req.body.requestId = req.params.requestId;
         } else {
             req.body.requestId = 0;
         }
         this.services.privateEthereumService.assignandremove(req, res, callback);
-        // call the azure message queue
     }
-
     // review the functions detail
     var review = function(req, res, callback) {
         if (req.params) {
@@ -68,17 +53,12 @@ module.exports = function() {
             req.body.requestId = 0;
         }
         this.services.privateEthereumService.review(req, res, callback);
-        // call the message queue
     }
-
     // revoke the contract
     var revoke = function(req, res, callback) {
-
         this.services.privateEthereumService.revoke(req, res, callback);
-        // call the message queue
     }
-
-     // change state of contract
+    // change state of contract
     var changestate = function(req, res, callback) {
         if (req.params) {
             req.body.requestId = req.params.requestId;
@@ -86,35 +66,25 @@ module.exports = function() {
             req.body.requestId = 0;
         }
         this.services.privateEthereumService.changestate(req, res, callback);
-        // call the message queue
     }
-
-
     var userdetail = function(req, res, callback) {
         Logger.info("userdetail");
         this.services.privateEthereumService.userdetail(req, res, callback);
-
     }
     var log = function(req, res, callback) {
-
         this.services.privateEthereumService.log(req, res, callback);
-
     }
     var fileModifylog = function(req, res, callback) {
-
         this.services.privateEthereumService.fileModifylog(req, res, callback);
-
     }
     var privateSendether = function(req, res, callback) {
         var reqData = req.body;
         this.services.accounts.privateSendether(req, res, callback);
     }
-
     var transactionConfirmations = function(req, res, callback) {
         var tranxHash = req.query.tranxHash;
         this.services.privateEthereumDetail.transactionConfirmations(tranxHash, res, callback);
     }
-
     var requestConfirmation = function(req, res, callback) {
         this.services.privateEthereumDetail.requestConfirmation(req, res, callback);
     }
@@ -122,54 +92,37 @@ module.exports = function() {
         var tranxHash = req.query.tranxHash;
         this.services.privateEthereumDetail.transactionDetail(tranxHash, res, callback);
     }
-
     var contractForAssets = function(req, res, callback) {
         var tranxHash = req.body.tranxHash;
         this.services.privateEthereumService.contractForAssets(tranxHash, res, callback);
-
     }
-
-    var sendHashIntransaction = function(req,res,callback){
-         this.services.simpleHashTransaction.sendHashIntransaction(req, res, callback);
+    var sendHashIntransaction = function(req, res, callback) {
+        this.services.simpleHashTransaction.sendHashIntransaction(req, res, callback);
     }
-
-    var broadcastTransactions = function(req,res,callback){
+    var broadcastTransactions = function(req, res, callback) {
         Logger.info('in broadcastTransactions controller ');
-         this.services.simpleHashTransaction.broadcastTransactions(req, res, callback);
+        this.services.simpleHashTransaction.broadcastTransactions(req, res, callback);
     }
-
-    var sendRawHashIntransaction = function(req,res,callback){
-         this.services.simpleHashTransaction.sendRawHashIntransaction(req, res, callback);
+    var sendRawHashIntransaction = function(req, res, callback) {
+        this.services.simpleHashTransaction.sendRawHashIntransaction(req, res, callback);
     }
-
-    var getHashIntransaction = function(req,res,callback){
-      this.services.simpleHashTransaction.getHashIntransaction(req, res, callback);
+    var getHashIntransaction = function(req, res, callback) {
+        this.services.simpleHashTransaction.getHashIntransaction(req, res, callback);
     }
-
     var saveFileAndGenerateHash = function(req, res, callback) {
         var myaccount = req.body.myAddress;
         var toAccount = req.body.toAddress;
         this.services.privateEthereumService.saveFileAndGenerateHash(toAccount, myaccount, req, res, callback);
     }
-
     var coinbaseBalance = function(req, res, callback) {
-
         this.services.privateEthereumDetail.coinbaseBalance(res, callback);
-
-
     }
-
-
-
     return {
-    //    insertBalance: insertBalance,
-      //  checkForRequest: checkForRequest,
         coinbaseBalance: coinbaseBalance,
         accountBalance: accountBalance,
-        sendHashIntransaction : sendHashIntransaction,
-        sendRawHashIntransaction : sendRawHashIntransaction,
-        getHashIntransaction : getHashIntransaction,
-        // privateImageHashGenerate: privateImageHashGenerate,
+        sendHashIntransaction: sendHashIntransaction,
+        sendRawHashIntransaction: sendRawHashIntransaction,
+        getHashIntransaction: getHashIntransaction,
         contractForAssets: contractForAssets,
         saveFileAndGenerateHash: saveFileAndGenerateHash,
         createAccount: createAccount,
@@ -178,21 +131,17 @@ module.exports = function() {
         transactionConfirmations: transactionConfirmations,
         assignandremove: assignandremove,
         smartContract: smartContract,
-        // privateImageHashtoContract: privateImageHashtoContract,
-        // smartPartyContract: smartPartyContract,
-        // sponsorPartyContract: sponsorPartyContract,
         review: review,
         fileModifylog: fileModifylog,
         revoke: revoke,
         changestate: changestate,
         userdetail: userdetail,
         log: log,
-        smartRawContract:smartRawContract,
+        smartRawContract: smartRawContract,
         latestBlock: latestBlock,
         requestConfirmation: requestConfirmation,
-        testContract:testContract,
-        createimportAccount : createimportAccount,
-        broadcastTransactions : broadcastTransactions
-
+        testContract: testContract,
+        createimportAccount: createimportAccount,
+        broadcastTransactions: broadcastTransactions
     }
 };
